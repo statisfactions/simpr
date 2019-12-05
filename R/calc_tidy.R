ef3977cd09a511ccd171c751f34c5e00dfc095cf
Ethan Brown <brow3821@umn.edu>
2019-12-05 12:36:31 -0600

    Refactor calc_tidy to call more abstract run_on_fit()

--- R/calc_tidy.R
+++ R/calc_tidy.R
@@ -70,22 +70,9 @@
 #'
 #' @export
 calc_tidy = function(simpr_mod) {
-  ## Create reference meta df for merging
-  simpr_meta = simpr_mod %>%
-    dplyr::select(tidyselect::one_of(c(attr(simpr_mod, "meta"), "rep"))) %>%
-    dplyr::mutate(....id = as.character(1:(dplyr::n())))
-
-  ## Extract all fit columns
-  simpr_mods = simpr_mod %>%
-    dplyr::select(tidyselect::one_of(c(attr(simpr_mod, "fits")))) %>%
-    purrr::map(purrr::set_names, nm = simpr_meta$....id)
-
-  ## For each fit column (identified as "source"), run tidy on each element of that column
-  simpr_tidy = purrr::map_dfr(simpr_mods, ~ purrr::map_dfr(., broom::tidy, .id = "....id"), .id = "Source")
-
-  ## Re-merge metaparameter columns to tidy output
-  dplyr::right_join(simpr_meta, simpr_tidy, by = "....id") %>%
-    dplyr::select(-....id)
-
+  ## Run broom::tidy() on fit columns in simpr_mod
+  run_on_fit(simpr_mod, broom::tidy)
 }
+
+
 
--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,25 @@
 #' @importFrom magrittr %>%
 #' @export
 magrittr::`%>%`
+
+
+# Run a given function on a simpr_mod object and tidy the output.
+run_on_fit = function(simpr_mod, FUN) {
+  ## Create reference meta df for merging
+  simpr_meta = simpr_mod %>%
+    dplyr::select(tidyselect::one_of(c(attr(simpr_mod, "meta"), "rep"))) %>%
+    dplyr::mutate(....id = as.character(1:(dplyr::n())))
+
+  ## Extract all fit columns
+  simpr_mods = simpr_mod %>%
+    dplyr::select(tidyselect::one_of(c(attr(simpr_mod, "fits")))) %>%
+    purrr::map(purrr::set_names, nm = simpr_meta$....id)
+
+  ## For each fit column (identified as "source"), run tidy on each element of that column
+  simpr_tidy = purrr::map_dfr(simpr_mods, ~ purrr::map_dfr(., FUN, .id = "....id"), .id = "Source")
+
+  ## Re-merge metaparameter columns to tidy output
+  dplyr::right_join(simpr_meta, simpr_tidy, by = "....id") %>%
+    dplyr::select(-....id)
+
+}
