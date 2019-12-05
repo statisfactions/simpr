e35713f5ea94f0551424df89bb88c631abc150ed
Ethan Brown <brow3821@umn.edu>
2019-12-04 19:34:53 -0600

    change first gen() argument to match meta()

--- R/gen.R
+++ R/gen.R
@@ -2,7 +2,7 @@
 #'
 #' Takes a simpr class object with metaparameters (e.g., output of simpr::variables() \%>\% simpr::meta()) and generates simulated data to be fit with a statistical model (e.g., lm().
 #'
-#' @param simpr simpr class object with metaparamters (e.g., output of simpr::variables() \%>\% simpr::meta())
+#' @param x simpr class object with metaparamters (e.g., output of simpr::variables() \%>\% simpr::meta())
 #' @param reps number of replications to run (integer)
 #'
 #' @examples
@@ -11,35 +11,35 @@
 #' @return Simulated data
 #'
 #' @export
-gen = function(simpr, reps) {
+gen = function(x, reps) {
   # Create data frame representing all possible values of meta parameter indices
-  specs = expand.grid(c(list(rep = 1:reps), simpr$meta$indices))
+  specs = expand.grid(c(list(rep = 1:reps), x$meta$indices))
 
 
-  if(!is.null(simpr$meta$lookup)) {
+  if(!is.null(x$meta$lookup)) {
     ## If there are list elements, join cells representing those list-columns
     ## into specs
-    specs = purrr::reduce2(simpr$meta$lookup,
+    specs = purrr::reduce2(x$meta$lookup,
                            inner_join,
                            .init = specs,
-                           .y = names(simpr$meta$lookup)) # the "by" argument to the join
+                           .y = names(x$meta$lookup)) # the "by" argument to the join
   }
 
   ## Generate all replications
   sim_results = specs %>%
-    dplyr::group_by_at(.vars = c("rep", names(simpr$meta$indices))) %>%
+    dplyr::group_by_at(.vars = c("rep", names(x$meta$indices))) %>%
     dplyr::do(sim_cell = purrr::pmap(., function(...) {
       eval_environment = rlang::as_environment(list(...), parent = parent.frame())
 
-      df = purrr::map_dfc(simpr$variables, function(x) {
+      df = purrr::map_dfc(x$variables, function(y) {
 
-        eval_fun = purrr::as_mapper(x)
+        eval_fun = purrr::as_mapper(y)
         environment(eval_fun) <- eval_environment
 
         gen = eval_fun() %>%
           unlist
 
-        varnames = attr(x, "varnames")
+        varnames = attr(y, "varnames")
 
         if(is.null(ncol(gen))) {
           gen_df = tibble::as_tibble(gen, .name_repair = "minimal")
@@ -51,7 +51,7 @@ gen = function(simpr, reps) {
         } else if(ncol(gen) == 0) {
           stop("variable function returns 0 columns")
         } else if(ncol(gen) == 1) {
-          names(gen) = attr(x, "varnames")
+          names(gen) = attr(y, "varnames")
           assign(varnames, gen[[1]], envir = eval_environment)
         } else if(ncol(gen) > 1) {
           gen_df = tibble::as_tibble(gen, .name_repair = "minimal")
@@ -64,7 +64,7 @@ gen = function(simpr, reps) {
             # Otherwise, use auto-numbering
             names(gen_df) = sprintf(paste0("%s%s%0", nchar(trunc(ncol(gen))), ".0f"),
                                     varnames,
-                                    attr(simpr$variables, "sep"),
+                                    attr(x$variables, "sep"),
                                     1:ncol(gen))
             ## assign names to the eval_environmnent
           }
@@ -82,8 +82,11 @@ gen = function(simpr, reps) {
     })) %>% tidyr::unnest(cols = c(sim_cell))
 
   ## Add some attributes to the tibble to track meta and variables
-  attr(sim_results, "meta") = names(simpr$meta$indices)
-  attr(sim_results, "variables") = purrr::map(simpr$variables, ~ attr(., "varnames")) %>% unlist
+  attr(sim_results, "meta") = names(x$meta$indices)
+  attr(sim_results, "variables") = purrr::map(x$variables, ~ attr(., "varnames")) %>% unlist
+
+  ## Add "simpr_gen" class
+  class(sim_results) = c("simpr_gen", class(sim_results))
 
   sim_results
 }
