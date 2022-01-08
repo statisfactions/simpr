e6d79340b58c47a2c7f43475bfc966480dbe47fb
Ethan Brown <brow3821@umn.edu>
2022-01-08 11:19:14 -0800

    optimization vignette (closes #48)

--- inst/doc/optimization.R
+++ inst/doc/optimization.R
@@ -4,6 +4,20 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
-## ----setup-----------------------------------------------------------------------------------------------------
+## ----setup, message=FALSE--------------------------------------------------------------------------------------
 library(simpr)
+
+## ----data_munging----------------------------------------------------------------------------------------------
+specify(control = ~ rnorm(n, mean = 0),
+        intervention_1 = ~ rnorm(n, mean = 0.2),
+        intervention_2 = ~ rnorm(n, mean = 2)) %>% 
+  define(n = c(6, 12)) %>% 
+  per_sim() %>% 
+  pivot_longer(cols = everything(),
+               names_to = "group", 
+               values_to = "response") %>% 
+  fit(lm = ~ lm(response ~ group)) %>% 
+  tidy_fits() %>% 
+  select(.sim_id, n, term, estimate) %>% 
+  generate(2) 
 
