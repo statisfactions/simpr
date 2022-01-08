71f6b6647c5d38fab5746c9bbc781605a3e16e59
Ethan Brown <brow3821@umn.edu>
2022-01-07 16:50:01 -0800

    add loadable vignettes and start on reproducibility

---
+++ inst/doc/reproducibility.R
@@ -0,0 +1,65 @@
+## ---- include = FALSE------------------------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup-----------------------------------------------------------------------------------
+library(simpr)
+
+## ----run_1-----------------------------------------------------------------------------------
+set.seed(500)
+run_1 = specify(a = ~ runif(6)) %>% 
+  generate(3)
+
+run_1
+
+## ----run_2-----------------------------------------------------------------------------------
+set.seed(500)
+run_2 = specify(a = ~ runif(6)) %>% 
+  generate(3)
+
+run_2
+
+## ----run_compare-----------------------------------------------------------------------------
+identical(run_1, run_2)
+
+## ----filter_after_generating-----------------------------------------------------------------
+set.seed(500)
+filter_after_generating = specify(a = ~ runif(6)) %>% 
+  generate(3) %>% 
+  filter(.sim_id == 2)
+
+filter_after_generating
+
+## ----filter_while_generating-----------------------------------------------------------------
+## Much faster, same result!
+set.seed(500)
+filter_while_generating = specify(a = ~ runif(6)) %>% 
+  generate(3, .sim_id == 2)
+
+filter_while_generating
+
+## ----filter_test-----------------------------------------------------------------------------
+identical(filter_after_generating, filter_while_generating)
+
+## ----filter_max_10---------------------------------------------------------------------------
+set.seed(500)
+filter_max_10 = specify(a = ~ sample(0:max, size = 10, replace = TRUE),
+        b = ~ a + rnorm(10))  %>% 
+  define(max = c(0, 1, 10)) %>%
+  generate(3, max == 10)
+
+filter_max_10
+
+## ----fit_error_data--------------------------------------------------------------------------
+fit_errors = filter(fit_tidy, !is.na(.fit_error))
+
+set.seed(500)
+fit_error_data = specify(a = ~ sample(1:max, size = 10, replace = TRUE),
+                     b = ~ a + rnorm(10))  %>% 
+  define(max = c(0, 1, 10)) %>%
+  generate(3, .sim_id %in% fit_errors$.sim_id)
+
+fit_error_data
+
---
+++ vignettes_workflow.R
@@ -0,0 +1,11 @@
+## Workflow for building vignettes so they show up
+## locally and without needing end-users on Github
+## to install
+
+## Taken from https://community.rstudio.com/t/browsevignettes-mypackage-saying-no-vignettes-found/68656/7
+tools::buildVignettes(dir = ".", tangle=TRUE)
+dir.create("inst/doc")
+file.copy(dir("vignettes", full.names=TRUE), "inst/doc", overwrite=TRUE)
+
+## Check package without deleting vignettes
+devtools::check(".", vignettes=FALSE)
